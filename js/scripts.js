04e65db382b7c1aaa8af2a7781716de8bc8dcbd4
aakeohane <75237371+aakeohane@users.noreply.github.com>
2021-01-07 15:03:49 -0800

    corrected syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-Let pokemonList = [
+let pokemonList = [
   {
     name: 'Vaporeon',
     type: ['water', 'ice'],
     weight: 63.9
-  };
+  },
   {
     name: 'Jolteon',
     type: ['electric', 'bug'],
     weight: 54
-  };
+  },
   {
     name: 'Flareon',
     type: ['fire', 'normal'],
     weight: 55.1
-  };
+  }
 ];
