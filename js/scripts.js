bb91f64039aa6f12491a33d2803723f7ef78347d
aakeohane <75237371+aakeohane@users.noreply.github.com>
2021-01-07 15:02:25 -0800

    Added pokemonList as array of objects of pokemon
    included name, weight and types in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'Mozzarella Sticks';
-document.write(favoriteFood);
+Let pokemonList = [
+  {
+    name: 'Vaporeon',
+    type: ['water', 'ice'],
+    weight: 63.9
+  };
+  {
+    name: 'Jolteon',
+    type: ['electric', 'bug'],
+    weight: 54
+  };
+  {
+    name: 'Flareon',
+    type: ['fire', 'normal'],
+    weight: 55.1
+  };
+];
