a623ef7ef34e5befe1cd034f82a9e3610c398678
aakeohane <75237371+aakeohane@users.noreply.github.com>
2021-01-07 15:46:27 -0800

    Added height key to each object

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,19 @@ let pokemonList = [
   {
     name: 'Vaporeon',
     type: ['water', 'ice'],
-    weight: 63.9
+    weight: 29,
+    height: 1
   },
   {
     name: 'Jolteon',
     type: ['electric', 'bug'],
-    weight: 54
+    weight: 24.5,
+    height: 0.8
   },
   {
     name: 'Flareon',
     type: ['fire', 'normal'],
-    weight: 55.1
+    weight: 25,
+    height: 0.9
   }
 ];
