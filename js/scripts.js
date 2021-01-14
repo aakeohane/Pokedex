4328028f47637e7a864a7bc9691a779afaedba58
aakeohane <75237371+aakeohane@users.noreply.github.com>
2021-01-14 13:47:05 -0800

    edited spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,41 @@
 let pokemonRepository = (function () {
-let pokemonList = [
-  {
-    name: 'Vaporeon',
-    type: ['water', 'ice'],
-    weight: 29,
-    height: 1
-  },
-  {
-    name: 'Jolteon',
-    type: ['electric', 'bug'],
-    weight: 24.5,
-    height: 0.8
-  },
-  {
-    name: 'Flareon',
-    type: ['fire', 'fox'],
-    weight: 25,
-    height: 0.9
-  }
-];
+  let pokemonList = [
+    {
+      name: 'Vaporeon',
+      type: ['water', 'ice'],
+      weight: 29,
+      height: 1
+    },
+    {
+      name: 'Jolteon',
+      type: ['electric', 'bug'],
+      weight: 24.5,
+      height: 0.8
+    },
+    {
+      name: 'Flareon',
+      type: ['fire', 'fox'],
+      weight: 25,
+      height: 0.9
+    }
+  ];
 
-function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
-function getAll() {
-    return pokemonList;
+  function add(pokemon) {
+      pokemonList.push(pokemon);
   }
 
-return {
-    add: add,
-    getAll: getAll
+  function getAll() {
+      return pokemonList;
+  }
+
+  return {
+      add: add,
+      getAll: getAll
   };
-})();
+  })();
 
 
 //  Lists pokemon by name and their height and if statement declares that they are tall
-//  Calls pokemonList through IIFE
 
 pokemonRepository.getAll().forEach(function (pokemon) {
     if (pokemon.height >= 1){
